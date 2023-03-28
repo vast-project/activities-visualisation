6038fbe588fc30f84978a83809d378c87d390898
nchrisdesigner <nchrisdesigner@gmail.com>
2023-03-28 20:38:25 +0300

    Docker Fixes

---
+++ systemd/stop-services.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ACTIVITIES_VISUALISATION_DIR=$( dirname  "$SCRIPT_DIR" )
+
+ACTIVITIES=(`ls -d --hide=".*/systemd/" -- "$ACTIVITIES_VISUALISATION_DIR"/*/`)
+
+SCRIPT="stop.sh"
+
+for path in ${ACTIVITIES[@]}; do
+  FILE="${path}docker/$SCRIPT"
+  TOOL=$( basename "$path" )
+  if [ -f "$FILE" ]; then
+    echo "Starting Service: $TOOL"
+    /bin/bash "$FILE"
+  fi
+done
+
