a7af344ea3ee89b441074619a7b17b80464fedd6
nchrisdesigner <nchrisdesigner@gmail.com>
2023-03-28 20:32:58 +0300

    Docker Fixes

--- backend-httprequests/config/env.js
+++ backend-httprequests/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'tasks_f755',
     username: 'tasksuser_x354',
     password: 'uWb47SYz5isC07qWAEYEiu',
-    host: 'localhost',
+    host: 'db',
     dialect: 'postgres',
     pool: {
         max: 5,
