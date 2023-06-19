80a08a01dd1a3a3e11b00381e9ca8e88a285a16e
Christina Damianou <anapodi84@gmail.com>
2023-06-19 10:20:43 +0300

    Docker: added a script to see logs

---
+++ docker/logs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+if command -v podman-compose &> /dev/null; then
+    export DOCKER_COMPOSE=podman-compose
+else
+    export DOCKER_COMPOSE=docker-compose
+fi
+
+${DOCKER_COMPOSE} --project-name activity-visualisation-imss-mindmap \
+                  -f docker-compose.yml \
+		  logs backend-admin
