6230726facbec19402d498cb868f82e44e464197
nchrisdesigner <66793672+nchrisdesigner@users.noreply.github.com>
2023-07-09 12:05:56 +0300

    Update Form.js

--- imss-mindmap/components/Form/Form.js
+++ imss-mindmap/components/Form/Form.js
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router'
 import styles from './form.module.css'
 import Image from 'next/image'
 import logo from '../../public/logo.png'
-import italy from '../../public/ita-flag.png'
-import uk from '../../public/uk-flag.png'
+import italy from '../../public/italy-flag.png'
+import uk from '../../public/eng-flag.png'
 import {useForm} from 'react-hook-form'
 import Mindmap from '../Mindmap/Mindmap'
 import Loading from '../Loading/Loading'
@@ -38,34 +38,112 @@ function Form() {
   const [isValid, setIsValid] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [isItalian, setIsItalian] = useState(true);
-  //const [jsondata,setJsonData] = useState({
-  var jsondata = {
-    name: "Maria Papa",
-    userid: null,
-    created_by: 2,
-    date_of_visit: null,
-    age: null,
-    gender: null,
-    education: null,
-    nationality: null,
-    motherLanguage: null,
-    activity: null,
-    group: null,
-    school: null
+
+  const [visitorData, setVisitorData] = useState({
+    name: '',
+    age: '',
+    gender: '',
+    date_of_visit: '',
+    nationality: '',
+    mother_language: '',
+    activity: '',
+    visitor_group: '',
+    school: '',
+  });
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  
+  //   const apiUrl = 'https://activities-backend.vast-project.eu/rest/visitors/';
+  
+  //   const requestData = {
+  //     name: visitorData.name,
+  //     age: visitorData.age,
+  //     gender: visitorData.gender,
+  //     date_of_visit: visitorData.date_of_visit,
+  //     nationality: visitorData.nationality,
+  //     mother_language: visitorData.mother_language,
+  //     activity: visitorData.activity,
+  //     visitor_group: visitorData.visitor_group,
+  //     school: visitorData.school
+  //   };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Define the endpoint URL
+    const apiUrl = 'https://activities_backend.vast-project.eu/rest/visitors/';
+
+    // Send the POST request
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(visitorData),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Visitor created:', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
+      .finally(() => setIsValid(true));
   };
 
+  // Handle Change Function for every input
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setVisitorData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+    // console.log('Updated visitor data:', visitorData);
+  };
+
+  //const [jsondata,setJsonData] = useState({
+
+  // Temporarily commented
+  // -----------------
+  // var jsondata = {
+  //   name: "Maria Papa",
+  //   userid: null,
+  //   created_by: 2,
+  //   date_of_visit: null,
+  //   age: null,
+  //   gender: null,
+  //   education: null,
+  //   nationality: null,
+  //   motherLanguage: null,
+  //   activity: null,
+  //   group: null,
+  //   school: null
+  // };
+  // -----------------------
+
+  // Dynamic Media Query for sceen width
   const [width, setWidth] = useState(0);
   const breakpoint = 700;
 
   const handleWindowResize = () => {
     setWidth(window.innerWidth);
   }
+
+  // Handle Language Functions
   const handleSetItalian = () => {
     setIsItalian(true);
   }
   const handleSetEnglish = () => {
     setIsItalian(false);
   }
+
+  // ---------------------------------
   var router = useRouter();
   var vschool = router.query["school"];
   var vmuseum = router.query["museum"];
@@ -82,6 +160,7 @@ function Form() {
   const educlevelRef = useRef(veduclevel);
   const ageRef = useRef(vage);
   const genderRef = useRef(null);
+  // --------------------------------------------
   
   useEffect(() => {
     handleWindowResize();
@@ -90,35 +169,38 @@ function Form() {
     return window.removeEventListener("resize", () => setWidth(window.innerWidth))
   }, [width]);
 
-  const handleSubmit = event => {
-    if (vgroupid==undefined || vgroupid==null)
-      {vgroupid=0;}
-    else
-      {vgroupid = Number(vgroupid.substring(0,vgroupid.length-1));}
-    if (vactivityid==undefined || vactivityid==null)
-      {vactivityid=0;}
-    else
-      {vactivityid = Number(vactivityid);}
-    jsondata={
-      name: nameRef.current.value,
-      created_by: 2,
-      age: ageRef.current.value,
-      date_of_visit: dateRef.current.value,
-      gender: genderRef.current.value,
-      education: educlevelRef.current.value,
-      motherLanguage: 2,
-      activity: vactivityid,
-      group: vgroupid,
-      school: schoolRef.current.value,
-      nationality: 2
-    }
-    console.log("JSON DATA")
-    console.log(JSON.stringify(jsondata))
-    var res = savevisitor(jsondata)
-    console.log(res)
-    setIsValid(true)
-    event.target.reset()
-  }
+
+  // ----------------------------------------
+  // const handleSubmit = event => {
+  //   if (vgroupid==undefined || vgroupid==null)
+  //     {vgroupid=0;}
+  //   else
+  //     {vgroupid = Number(vgroupid.substring(0,vgroupid.length-1));}
+  //   if (vactivityid==undefined || vactivityid==null)
+  //     {vactivityid=0;}
+  //   else
+  //     {vactivityid = Number(vactivityid);}
+  //   jsondata={
+  //     name: nameRef.current.value,
+  //     created_by: 2,
+  //     age: ageRef.current.value,
+  //     date_of_visit: dateRef.current.value,
+  //     gender: genderRef.current.value,
+  //     education: educlevelRef.current.value,
+  //     motherLanguage: 2,
+  //     activity: vactivityid,
+  //     group: vgroupid,
+  //     school: schoolRef.current.value,
+  //     nationality: 2
+  //   }
+  //   console.log("JSON DATA")
+  //   console.log(JSON.stringify(jsondata))
+  //   var res = savevisitor(jsondata)
+  //   console.log(res)
+  //   setIsValid(true)
+  //   event.target.reset()
+  // }
+  // ------------------------------------------
 
   useEffect(() => {
     setTimeout(() => {
@@ -136,19 +218,25 @@ function Form() {
     return (
       
       <motion.div transition={{ duration: 1 }} initial={{ opacity: 0 }} animate={{ opacity: 1}} className={styles.container}>
+
         <h1 className={styles.primary}>{isItalian ? 'Benvenuti al' : 'Welcome to' } <span className={styles.bold}>Museo Galileo</span></h1>
         <h2 className={styles.secondary}>{isItalian ? 'Goditi la tua visita al nostro museo' : 'Enjoy your visit to our museum'}</h2>
         <div className={styles.boldline}></div>
-  
+
+        {/* Form */}
         <form className={styles.form} onSubmit={handleSubmit} >  
+
+          {/* Name */}
           <div className={styles.inputContainer}>
             <label>{isItalian ? "Il nome del visitatore" : "Visitor's Name"}</label>
-            <input ref={nameRef} required id="inputname" type="text" label={isItalian ? "Il nome del visitatore" : "Visitor's Name"} />
+            <input ref={nameRef} required id="inputname" type="text" label={isItalian ? "Il nome del visitatore" : "Visitor's Name"} name="name" value={visitorData.name} onChange={handleChange} />
           </div>
+
+          {/* Age */}
           <div className={styles.ageContainer}>
-            <label for="inputage">{isItalian ? "Età" : "Age"}</label>
-            <select id="inputage" name="inputage" ref={ageRef} defaultValue={vage} required>
-              <option value="">{isItalian ? "Selezionare  Età..." : "Select Age..."}</option>
+            <label>{isItalian ? "Età" : "Age"}</label>
+            <select  name="age" ref={ageRef} defaultValue={vage} value={visitorData.age} onChange={handleChange} required>
+              <option >{isItalian ? "Selezionare  Età..." : "Select Age..."} </option>
               <option value="14-15">14-15</option>
               <option value="15-16">15-16</option>
               <option value="16-17">16-17</option>
@@ -156,29 +244,50 @@ function Form() {
               <option value="18-19">18-19</option>
             </select>
           </div>
+
+          {/* Gender */}
           <div className={styles.ageContainer}>
             <label>{isItalian ? "Il genere del visitatore" : "Visitor's Gender"}</label>
-            <select id="inputgender" name="inputgender" ref={genderRef} required>
+            <select value={visitorData.gender} name="gender" onChange={handleChange} required >
               <option value="">{isItalian ? "Selezionare  Genere..." : "Select Gender..."}</option>
               <option value="Male">{isItalian ? "Maschio" : "Male"}</option>
               <option value="Female">{isItalian ? "Maschia" : "Female"}</option>
             </select>
           </div>
+
+          {/* Date */}
           <div className={styles.inputContainer}>
             <label>{isItalian ? "Data/Tempo del visitatore" : "Date/Time of Visit"}</label>
-            <input ref={dateRef} required id="inputtime" type="datetime-local" label={isItalian ? "Data/Tempo del visitatore" : "Date/Time of Visit"} />
-            </div>
+            <input ref={dateRef} required id="inputtime" type="datetime-local" label={isItalian ? "Data/Tempo del visitatore" : "Date/Time of Visit"} name="date_of_visit" value={visitorData.date_of_visit} onChange={handleChange} />
+          </div>
+
+          {/* Nationality */}
           <div className={styles.inputContainer}>
-            <label>{isItalian ? "Il nome della scuola" : "School Name"}</label>
-            <input ref={schoolRef} required id="inputschool" type="text" label={isItalian ? "Il nome della scuola" : "School Name"} defaultValue={vschool} />
-            </div>
+            <label>{isItalian ? "Nazionalità" : "Nationality"}</label>
+            <input required name="nationality" value={visitorData.nationality} onChange={handleChange} />
+          </div>
+
+          {/* Language */}
           <div className={styles.inputContainer}>
-            <label>{isItalian ? "Museo" : "Museum"}</label>
-            <input ref={museumRef} required id="inputmuseum" type="text" label={isItalian ? "Museo" : "Museum"} defaultValue="Museo Galileo - Instituto e Museo di Storia della Scienza" />
-            </div>
+            <label>{isItalian ? "Lingua" : "Language"}</label>
+            <input required type="text" name="mother_language" value={visitorData.mother_language} onChange={handleChange} />
+          </div>
+
+          {/* Activity */}
           <div className={styles.inputContainer}>
-            <label>{isItalian ? "Livello educativo" : "Educational Level"}</label>
-            <input ref={educlevelRef} required id="inputeduclevel" type="text" label={isItalian ? "Livello educativo" : "Educational Level"} defaultValue="Scuola secondaria di secondo grado" /> 
+            <label>{isItalian ? "Attività" : "Activity"}</label>
+            <input required type="text" name="activity" value={visitorData.activity} onChange={handleChange} /> 
+          </div>
+          {/* Visitor Group */}
+          <div className={styles.inputContainer}>
+            <label>{isItalian ? "Gruppo di visitatori" : "Visitor Group"}</label>
+            <input required type="text" name="visitor_group" value={visitorData.visitor_group} onChange={handleChange} /> 
+          </div>
+
+          {/* School */}
+          <div className={styles.inputContainer}>
+            <label>{isItalian ? "Scuola" : "School"}</label>
+            <input required type="text" name="school" value={visitorData.school} onChange={handleChange} /> 
           </div>
 
           <button className={styles.submitBtn} type="submit">{isItalian ? "Benvenuto" : "Welcome"}</button>
@@ -196,13 +305,11 @@ function Form() {
         </div>
       </motion.div>
     )
-
   }
 
   return (
     width > breakpoint ? <Mappa isItalian={isItalian} setIsItalian={setIsItalian} /> : <Mindmap isItalian={isItalian} setIsItalian={setIsItalian} />
   )
-  
 }
 
 export default Form
