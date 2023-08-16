c911a4f0f1325b261ef4e4a62587f0dd28b42510
Georgios Petasis <petasis@iit.demokritos.gr>
2023-08-16 21:47:16 +0300

    Django: Dashboard

--- activities-backend/digitisation/filters.py
+++ activities-backend/digitisation/filters.py
@@ -2,18 +2,44 @@ from django.db.models import Q
 import django_filters
 from activity_data.models import *
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit, Row, Column
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+from django.utils.safestring import mark_safe
+
 class VASTObjectFilter(django_filters.FilterSet):
     query = django_filters.CharFilter(method='universal_search',
                                       label="")
 
-    class Meta:
-        model = VASTObject
-        fields = ['query']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form.helper = FormHelper()
+        self.form.helper.layout = Layout(
+          Row(
+            Column(
+              PrependedText('query',
+                mark_safe('<i class="fas fa-search" aria-hidden="true"></i>'),
+                placeholder="Type here...",
+              ),
+              css_class="col-lg-8 col-md-8 ms-3",
+            ),
+            Column(
+              FormActions(
+                Submit('submit', 'Search'),
+              ),
+              css_class="col-lg-2 col-md-2",
+            ),
+          )
+        )
 
     def universal_search(self, queryset, name, value):
         return self._meta.model.objects.filter(
             Q(name__icontains=value)
         )
+
+    class Meta:
+        model = VASTObject
+        fields = ['query']
 
 class ActivityFilter(VASTObjectFilter):
     class Meta(VASTObjectFilter.Meta):
