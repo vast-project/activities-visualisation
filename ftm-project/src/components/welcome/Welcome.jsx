b021c9e3d81b992f84b3c6fb7409955760561c43
Leonidas Tsekouras <leotsekouras@gmail.com>
2023-09-06 19:59:31 +0300

    FTM: Make welcome screen support language changes

--- ftm-project/src/components/welcome/Welcome.jsx
+++ ftm-project/src/components/welcome/Welcome.jsx
@@ -4,31 +4,50 @@ import Paragraph from './../../ui/Paragraph/Paragraph'
 import Button from './../../ui/Button/Button'
 import Before from './../questionnere/before/Before'
 
-import { useState, useContext } from 'react'
-import { LangContext } from '../../layout/Layout'
-
-const text = "Η δραστηριότητα «Οι Αξίες στα Παραμύθια» μέσα από ένα ταξίδι στον μαγικό κόσμο των παραμυθιών και στο πως αυτά δημιουργούνται αλλά και μέσα από την ανασκαφή των αξιών που απορρέουν από αυτά παρασύρει τους εφήβους σε μια ενδοσκόπηση για το πως οι αξίες αυτές διέπουν την καθημερινότητα τους αλλά και την κοινωνία στην οποία ζούν."
+import {useContext, useState} from 'react'
+import {LangContext} from '../../layout/Layout'
 
 const Welcome = () => {
-  const {isEnglish} = useContext(LangContext)
-  const [next,setNext] = useState(false)
+    const {isEnglish} = useContext(LangContext);
+    const [next, setNext] = useState(false);
 
-  if(next) {
+    const titleText = {
+        en: "Values in Fairy Tales",
+        gr: "Οι Αξιες στα Παραμύθια"
+    }
+
+    const welcomeText = {
+        en: "Welcome",
+        gr: "Καλώς ήλθατε"
+    }
+
+    const nextBtnText = {
+        en: "NEXT",
+        gr: "ΕΠΟΜΕΝΟ"
+    }
+
+    const text = {
+        en: "The activity “Values in Fairy Tales” through a journey to the magical world of fairy tales and how they are created but also through the excavation of the values that derive from them, it leads teenagers to an introspection of how these values govern their daily lives and the society in which they live.",
+        gr: "Η δραστηριότητα «Οι Αξίες στα Παραμύθια» μέσα από ένα ταξίδι στον μαγικό κόσμο των παραμυθιών και στο πως αυτά δημιουργούνται αλλά και μέσα από την ανασκαφή των αξιών που απορρέουν από αυτά παρασύρει τους εφήβους σε μια ενδοσκόπηση για το πως οι αξίες αυτές διέπουν την καθημερινότητα τους αλλά και την κοινωνία στην οποία ζούν."
+    }
+
+    if (next) {
+        return (
+            <>
+                <Before/>
+            </>
+        )
+    }
+
     return (
-      <>
-      <Before />
-      </>
+        <>
+            <Title title={isEnglish ? titleText.en : titleText.gr}/>
+            <SubTitle title={isEnglish ? welcomeText.en : welcomeText.gr}/>
+            <Paragraph text={isEnglish ? text.en : text.gr}/>
+            <Button onClick={() => setNext(true)} text={isEnglish ? nextBtnText.en : nextBtnText.gr}
+                    color="rgb(105, 160, 130)"/>
+        </>
     )
-  }
-
-  return (
-    <>
-      <Title title="Οι Αξιες στα Παραμύθια" />
-      <SubTitle title="Καλώς ήλθατε" />
-      <Paragraph text={text} />
-      <Button onClick={() => setNext(true)} text="ΕΠΟΜΕΝΟ" color="rgb(105, 160, 130)" />
-    </>
-  )
 }
 
 export default Welcome
