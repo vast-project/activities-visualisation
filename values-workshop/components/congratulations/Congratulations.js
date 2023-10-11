4b10148b4ff04725a847195a99aa3b8f8c81d0cc
Leonidas Tsekouras <leotsekouras@gmail.com>
2023-10-11 17:29:38 +0300

    Values: Add "saving data..." text to last page

--- values-workshop/components/congratulations/Congratulations.js
+++ values-workshop/components/congratulations/Congratulations.js
@@ -1,18 +1,22 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import styles from './congratulations.module.css'
 import {IdContext} from '../welcome/Welcome'
 import {LangContext} from "../layout/Layout";
+import {DotLoader} from "react-spinners";
 
 
 function Congratulations({annotations, selectedValuesFromFirst, selectedValuesFromSecond, selectedValuesFromThird}) {
     const {isEnglish, setIsEnglish} = useContext(LangContext)
     const userId = useContext(IdContext);
+    const [saving, setSaving] = useState(true);
 
     console.log("User ID", userId)
     console.log("Annotations", annotations)
     console.log("1st values", selectedValuesFromFirst)
     console.log("2nd values", selectedValuesFromSecond)
     console.log("3rd values", selectedValuesFromThird)
+
+    // todo: Save the data to the api, and then set saving to false!
 
     return (
         <div className={styles.container}>
@@ -45,6 +49,20 @@ function Congratulations({annotations, selectedValuesFromFirst, selectedValuesFr
                     })}
                 </div>
             </div>
+            {saving ?
+                <div>
+                    <div className={styles.dotLoader}>
+                        <DotLoader color="#36d7b7"/>
+                    </div>
+                    <div className={styles.savingText}>
+                        <span>{isEnglish ? "Saving data..." : "Αποθήκευση δεδομένων..."}</span>
+                    </div>
+                </div>
+                :
+                <div className={styles.savingText}>
+                    <span>{isEnglish ? "Thank you for participating!" : "Ευχαριστούμε για τη συμμετοχή!"}</span>
+                </div>
+            }
         </div>
     )
 }
