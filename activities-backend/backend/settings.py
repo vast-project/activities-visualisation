a118c83883574b960b8c709b70e66f502bf82dc7
Georgios Petasis <petasis@iit.demokritos.gr>
2023-10-13 07:51:26 +0300

    Django: Logout Redirect

--- activities-backend/backend/settings.py
+++ activities-backend/backend/settings.py
@@ -91,6 +91,10 @@ INSTALLED_APPS = [
     'django_cleanup.apps.CleanupConfig', # This must be always at the end!
 ]
 
+LOGIN_URL="/accounts/login/"
+LOGIN_REDIRECT_URL="/"
+LOGOUT_REDIRECT_URL="/accounts/login/"
+
 MATOMO_SITE_ID=1
 MATOMO_URL='https://matomo.vast-project.eu/'
 
