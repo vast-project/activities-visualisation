f3b84e22a2e8f8387fb1ba4decf9a54a5e85ea29
Leonidas Tsekouras <leotsekouras@gmail.com>
2023-10-28 13:04:53 +0300

    Values: Add questionnaire to questionnaire step

--- values-workshop/components/questionnaire/Questionnaire.js
+++ values-workshop/components/questionnaire/Questionnaire.js
@@ -4,13 +4,15 @@ import React, {useContext, useState} from 'react';
 import Button from '@component/ui/button/Button';
 import {LangContext} from "../layout/Layout";
 import Instructions from "@component/components/instructions/Instructions";
-import Welcome from "@component/components/welcome/Welcome";
+import Welcome, {IdContext} from "@component/components/welcome/Welcome";
 
 
 function Questionnaire() {
     const {isEnglish, setIsEnglish} = useContext(LangContext)
     const [next, setNext] = useState(false);
     const [previous, setPrevious] = useState(false);
+
+    const questionnaireBaseUrl = 'https://platform.vast-project.eu/values-portait-questionnaire-rur-en/?participant_id='
 
     if (next) {
         return <Instructions/>
@@ -19,11 +21,17 @@ function Questionnaire() {
         return <Welcome/>
     }
 
+    // Get user id and use it to create the questionnaire URL
+    const userId = useContext(IdContext);
+    const questionnaireUrl = questionnaireBaseUrl + encodeURIComponent(userId);
+
     return (
         <div className={styles.container}>
             <BsArrowLeftCircleFill onClick={() => setPrevious(true)} className={styles.leftArrow}/>
             <h1 className={styles.primaryHeadline}>{isEnglish ? 'Questionnaire' : 'Ερωτηματολόγιο'}</h1>
             <h3 className={styles.secondaryHeadline}>{isEnglish ? 'Please answer the following questions' : 'Παρακαλώ απαντήστε στις παρακάτω ερωτήσεις'}</h3>
+
+            <iframe className={styles.iframe} src={questionnaireUrl} title="Questionnaire"></iframe>
 
             <Button onClick={() => setNext(!next)} color="#FFC857" title={isEnglish ? 'Next' : "Επομενο"}/>
         </div>
